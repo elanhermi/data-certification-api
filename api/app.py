fe24391bb59bf4f297df2897cbb49bc11f128d8e
Helene Herman <hlne.herman@gmail.com>
2021-03-19 17:54:07 +0100

    Beautiful code

--- api/app.py
+++ api/app.py
@@ -1,13 +1,39 @@
 
 from fastapi import FastAPI
+import requests
+import joblib
+import json
 
 app = FastAPI()
 
+with open("model.joblib") as f:
+        my_model = joblib.load(f)
 
-# define a root `/` endpoint
-@app.get("/")
-def index():
-    return {"ok": True}
+@app.get("/predict")
+
+def predict_popularity():
+    url = 'http://localhost:8000/predict'
+    params = {
+    'acousticness': 0.654,
+    'danceability': 0.499,
+    'duration_ms': 219827,
+    'energy': 0.19,
+    'explicit': 0,
+    'id': '0B6BeEUd6UwFlbsHMQKjob',
+    'instrumentalness': 0.00409,
+    'key': 7,
+    'liveness': 0.0898,
+    'loudness': 16.435,
+    'mode': 1,
+    'name': 'Back in the Goodle Days',
+    'release_date': 1971,
+    'speechiness': 0.0454,
+    'tempo': 149.46,
+    'valence': 0.43, 
+    'artist': 'John Hartford'
+    }
+    response = requests.get(url, params=params)
+    return response.json()
 
 
 # Implement a /predict endpoint
